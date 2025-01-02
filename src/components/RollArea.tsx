f9af43f31db4895b1da7ade1801695329c583131
Josie Robin de Oliveira Guimarães <100611122+MiauJosie@users.noreply.github.com>
2025-01-02 14:32:16 -0300

    wheelEvent now adds/subtracts 5 instead of 1

--- src/components/RollArea.tsx
+++ src/components/RollArea.tsx
@@ -5,27 +5,27 @@ import CurrentRoll from "./RollArea/CurrentRoll";
 import { Context } from "../App";
 
 function RollArea() {
-  const { noDiceSelected, blablabla } = useContext(Context);
-  return (
-    <div className="RollArea">
-      {noDiceSelected ? (
-        <div
-          className="RollAlert"
-          style={{
-            transform: blablabla ? "" : "translateY(-12rem)",
-          }}
-        >
-          <h3>No dice selected</h3>
+    const { noDiceSelected, blablabla } = useContext(Context);
+    return (
+        <div className="RollArea">
+            {noDiceSelected ? (
+                <div
+                    className="RollAlert"
+                    style={{
+                        transform: blablabla ? "" : "translateY(-12rem)",
+                    }}
+                >
+                    <h3>No dice selected</h3>
+                </div>
+            ) : null}
+            <div className="Title">
+                <h1>Mirelucky</h1>
+                <h2>Fallout 2d20 Dice Roller</h2>
+            </div>
+            <CurrentRoll />
+            <RollButton />
         </div>
-      ) : null}
-      <div className="Title">
-        <h1>Mirelucky</h1>
-        <h2>Fallout 2d20 Dice Roller</h2>
-      </div>
-      <CurrentRoll />
-      <RollButton />
-    </div>
-  );
+    );
 }
 
 export default RollArea;
--- src/components/RollArea/RollOptions.tsx
+++ src/components/RollArea/RollOptions.tsx
@@ -8,103 +8,103 @@ import combatImg from "../../assets/combatdice.png";
 import locationImg from "../../assets/locationdice.png";
 
 function RollOptions() {
-  const [bounces, setBounces] = useState<boolean[]>([false, false, false]);
-  const [setRollAvailable] = useContext(RollAvailable);
-  const { count, setCount } = useContext(Context);
+    const [bounces, setBounces] = useState<boolean[]>([false, false, false]);
+    const [setRollAvailable] = useContext(RollAvailable);
+    const { count, setCount } = useContext(Context);
 
-  useEffect(() => {
-    setRollAvailable(count[0] > 0 || count[1] > 0 || count[2] > 0);
-  }, [count]);
+    useEffect(() => {
+        setRollAvailable(count[0] > 0 || count[1] > 0 || count[2] > 0);
+    }, [count]);
 
-  const handleContextMenu = (index: number, event: React.MouseEvent) => {
-    event.preventDefault();
+    const handleContextMenu = (index: number, event: React.MouseEvent) => {
+        event.preventDefault();
 
-    const newCount = [...count];
-    count[index] > 0 ? (newCount[index] -= 1) : null;
-    setCount(newCount);
+        const newCount = [...count];
+        count[index] > 0 ? (newCount[index] -= 1) : null;
+        setCount(newCount);
 
-    animate(index);
-  };
+        animate(index);
+    };
 
-  const handleClick = (index: number, event: React.MouseEvent) => {
-    event.preventDefault();
+    const handleClick = (index: number, event: React.MouseEvent) => {
+        event.preventDefault();
 
-    const newCount = [...count];
-    newCount[index] += 1;
-    setCount(newCount);
+        const newCount = [...count];
+        newCount[index] += 1;
+        setCount(newCount);
 
-    animate(index);
-  };
+        animate(index);
+    };
 
-  const handleWheel = (index: number, event: React.WheelEvent) => {
-    if (event.deltaY > 0) {
-      const newCount = [...count];
-      count[index] > 0 ? (newCount[index] -= 1) : null;
-      setCount(newCount);
+    const handleWheel = (index: number, event: React.WheelEvent) => {
+        if (event.deltaY > 0) {
+            const newCount = [...count];
+            count[index] >= 5 ? (newCount[index] -= 5) : null;
+            setCount(newCount);
 
-      animate(index);
-    } else {
-      const newCount = [...count];
-      newCount[index] += 1;
-      setCount(newCount);
+            animate(index);
+        } else {
+            const newCount = [...count];
+            newCount[index] += 5;
+            setCount(newCount);
 
-      animate(index);
-    }
-  };
+            animate(index);
+        }
+    };
 
-  const animate = (index: number) => {
-    setBounces((prevBounces) => {
-      const newBounces = [...prevBounces];
-      newBounces[index] = true;
-      return newBounces;
-    });
+    const animate = (index: number) => {
+        setBounces((prevBounces) => {
+            const newBounces = [...prevBounces];
+            newBounces[index] = true;
+            return newBounces;
+        });
 
-    setTimeout(() => {
-      setBounces((prevBounces) => {
-        const newBounces = [...prevBounces];
-        newBounces[index] = false;
-        return newBounces;
-      });
-    }, 100);
-  };
+        setTimeout(() => {
+            setBounces((prevBounces) => {
+                const newBounces = [...prevBounces];
+                newBounces[index] = false;
+                return newBounces;
+            });
+        }, 100);
+    };
 
-  return (
-    <div className="RollOptions">
-      <div className="OptionWrapper">
-        <h2>{count[0]}</h2>
-        <img
-          onClick={(event) => handleClick(0, event)}
-          onContextMenu={(event) => handleContextMenu(0, event)}
-          onWheel={(event) => handleWheel(0, event)}
-          className={bounces[0] ? "bounce" : ""}
-          src={d20Img}
-          alt="d20"
-        />
-      </div>
-      <div className="OptionWrapper">
-        <h2>{count[1]}</h2>
-        <img
-          onClick={(event) => handleClick(1, event)}
-          onContextMenu={(event) => handleContextMenu(1, event)}
-          onWheel={(event) => handleWheel(1, event)}
-          className={bounces[1] ? "bounce" : ""}
-          src={combatImg}
-          alt="combat dice"
-        />
-      </div>
-      <div className="OptionWrapper">
-        <h2>{count[2]}</h2>
-        <img
-          onClick={(event) => handleClick(2, event)}
-          onContextMenu={(event) => handleContextMenu(2, event)}
-          onWheel={(event) => handleWheel(2, event)}
-          className={bounces[2] ? "bounce" : ""}
-          src={locationImg}
-          alt="location dice"
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="RollOptions">
+            <div className="OptionWrapper">
+                <h2>{count[0]}</h2>
+                <img
+                    onClick={(event) => handleClick(0, event)}
+                    onContextMenu={(event) => handleContextMenu(0, event)}
+                    onWheel={(event) => handleWheel(0, event)}
+                    className={bounces[0] ? "bounce" : ""}
+                    src={d20Img}
+                    alt="d20"
+                />
+            </div>
+            <div className="OptionWrapper">
+                <h2>{count[1]}</h2>
+                <img
+                    onClick={(event) => handleClick(1, event)}
+                    onContextMenu={(event) => handleContextMenu(1, event)}
+                    onWheel={(event) => handleWheel(1, event)}
+                    className={bounces[1] ? "bounce" : ""}
+                    src={combatImg}
+                    alt="combat dice"
+                />
+            </div>
+            <div className="OptionWrapper">
+                <h2>{count[2]}</h2>
+                <img
+                    onClick={(event) => handleClick(2, event)}
+                    onContextMenu={(event) => handleContextMenu(2, event)}
+                    onWheel={(event) => handleWheel(2, event)}
+                    className={bounces[2] ? "bounce" : ""}
+                    src={locationImg}
+                    alt="location dice"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default RollOptions;
